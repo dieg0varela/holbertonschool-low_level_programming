0ee25d55f97b8ce67f834fcccdb8269431503077
Diego Varela <2806@holbertonschool.com>
2021-02-03 14:02:13 +0000

    Upload tasks 1-4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Show a message store in mensaje
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char mensaje[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(mensaje);
+	return (0);
+}
