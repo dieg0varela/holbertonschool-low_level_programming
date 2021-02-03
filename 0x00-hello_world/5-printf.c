a195f49489e752bc8e482d425f30d71f79cf0120
Diego Varela <2806@holbertonschool.com>
2021-02-03 16:34:37 +0000

    Upload tasks 5-6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: Show a message store en mensajito
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char mensajito[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", mensajito);
+	return (0);
+}
