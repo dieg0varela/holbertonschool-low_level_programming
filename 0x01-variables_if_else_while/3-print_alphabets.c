d69484e731fb7aebb652e68a68e048f113620b65
Diego Varela <2806@holbertonschool.com>
2021-02-04 16:36:16 +0000

    Upload tasks 2-3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: Show all te alphabet first in lowercase next to uppercase
+* Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+	char ch;
+
+	for(ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for(ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
