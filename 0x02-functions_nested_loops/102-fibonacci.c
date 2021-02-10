f23b7f23d06c3c65e3880c3fd8dddc8060ee0186
Diego Varela <2806@holbertonschool.com>
2021-02-10 02:35:34 +0000

    Upload task 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: Print the first 50 numbers of fibonacci secuence
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	long a, b, res, i;
+
+	a = 1;
+	b = 2;
+	printf("%ld, ", a);
+	printf("%ld, ", b);
+	for (i = 2 ; i <= 50 ; i++)
+	{
+		res = a + b;
+		printf("%ld", res);
+		if (i != 50)
+		{
+			printf(", ");
+		}
+		a = b;
+		b = res;
+	}
+	printf("\n");
+}
