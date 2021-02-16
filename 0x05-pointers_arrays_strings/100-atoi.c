7190b7a9361eb49ac6f62bf0de780069f8e8f465
Diego Varela <2806@holbertonschool.com>
2021-02-16 20:41:35 +0000

    Fix 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -30,19 +30,18 @@ int _atoi(char *s)
 {
 	int flag = 0, num = 0, foundnum = 0, i = 0;
 
-	int plus = 0, minus = 0, cifras = 0, j = 0;
+	int minus = 0, cifras = 0, j = 0;
 
 	while (!flag)
 	{
-		if (s[i] == '+')
-			plus++;
-		if (s[i] == '-')
+		if ((s[i] == '-') && (foundnum == 0))
 			minus++;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			foundnum = 1;
 			cifras++;
 		}
+
 		if (foundnum == 1 && (s[i] < '0' || s[i] > '9'))
 		{
 			flag = 1;
