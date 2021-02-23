69ea8910344d1d7b28e91d20b2355b1a3ec2883a
Diego Varela <2806@holbertonschool.com>
2021-02-23 20:49:34 +0000

    Upload task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+* _strpbrk - Entry point
+* @s: String
+* @accept: String
+*
+* Description: Search a string of any set of bytes
+* Return:csoo
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j, bool = 0;
+	char *r;
+
+	for (i = 0 ; s[i] != '\0'; i++)
+	{
+		for (j = 0 ; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				r = &s[i];
+				bool = 1;
+				break;
+			}
+		}
+		if (bool)
+			break;
+	}
+	return (r);
+}
+
+
+
+
+
+
