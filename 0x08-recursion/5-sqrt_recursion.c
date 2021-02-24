3b6b89724a859d98a2a0563190d6f9cc1e00297f
Diego Varela <2806@holbertonschool.com>
2021-02-24 19:46:19 +0000

    Upload task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+* aux - Entry point
+* @n: number
+* @i: number
+*
+* Description: hello
+* Return: int
+*/
+int aux(int n, int i)
+{
+	int r = 0;
+
+	if (i * i == n)
+		return (i);
+
+	if (i * i > n)
+		return (-1);
+
+	r = aux (n , i + 1);
+	return (r);
+}
+
+
+/**
+* _sqrt_recursion - Entry point
+* @n: number
+*
+* Description: Find the square root of n
+* Return: Int resault
+*/
+int _sqrt_recursion(int n)
+{
+	int res = 0;
+
+	res = aux(n, 1);
+	return (res);
+}
