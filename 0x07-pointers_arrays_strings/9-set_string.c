6ae2ab0ee79911ca0274904f5ff53f975745fbb1
Diego Varela <2806@holbertonschool.com>
2021-02-24 02:22:35 +0000

    Upload task 8

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* set_string - Entry point
+* @s: String of String
+* @to: String
+*
+* Description: Set dest to pointer of pointer
+* Return: Nothing
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
