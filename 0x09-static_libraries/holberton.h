b109539885e22e5863c2c521ced744dacd9fc434
Diego Varela <2806@holbertonschool.com>
2021-02-27 15:58:45 +0000

    Fix 0

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,15 +1,16 @@
+#include <stdio.h>
+
 int _putchar(char c);
-int _islower(int c);
-int _isalpha(int c);
-int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
-int _strlen(char *s);
-void _puts(char *s);
-char *_strcpy(char *dest, char *src);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int);
 int _atoi(char *s);
+int _strlen(char *s);
+void _puts(char *str);
+char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
