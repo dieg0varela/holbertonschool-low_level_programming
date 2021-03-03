67313921aad0f9789ce8fb665a33a0c9ebf542c0
Diego Varela <2806@holbertonschool.com>
2021-03-03 17:49:27 +0000

    Fix 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -22,35 +22,20 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (1);
 	}
-
 	cents = atoi(argv[1]);
 	while (cents)
 	{
 		if (cents >= 25)
-		{
-			cont++;
 			cents -= 25;
-		}
 		else if (cents >= 10)
-		{
-			cont++;
 			cents -= 10;
-		}
 		else if (cents >= 5)
-		{
-			cont++;
 			cents -= 5;
-		}
 		else if (cents >= 2)
-		{
-			cont++;
 			cents -= 2;
-		}
 		else if (cents >= 1)
-		{
-			cont++;
 			cents -= 1;
-		}
+		cont++;
 	}
 	printf("%d\n", cont);
 	return (0);
