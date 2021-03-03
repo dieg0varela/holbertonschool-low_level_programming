fb0f9453b0765f108c0026eeccd49fd070dbe72c
Diego Varela <2806@holbertonschool.com>
2021-03-03 17:41:20 +0000

    Fix 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,9 +12,14 @@ int main(int argc, char *argv[])
 {
 	int cont = 0, cents = 0;
 
-	if (argc != 2 || atoi(argv[1]) <= 0)
+	if (argc != 2)
 	{
 		printf("Error\n");
+		return (1);
+	}
+	if (atoi(argv[1]) <= 0)
+	{
+		printf("0\n");
 		return (1);
 	}
 
