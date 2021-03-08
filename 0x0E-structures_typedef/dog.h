e1330c98f1e525d6649ab06e3622466c96824feb
Diego Varela <2806@holbertonschool.com>
2021-03-08 16:41:12 +0000

    Upload Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+* struct dog - Structure for a dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: Owner of the dog
+*
+* Description: Info of a dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
