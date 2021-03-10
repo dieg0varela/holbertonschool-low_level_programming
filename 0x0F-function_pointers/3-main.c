a2a4d2b736d333644ea5adfc5a29b23f4147584a
Diego Varela <2806@holbertonschool.com>
2021-03-10 19:10:35 +0000

    Upload task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main- Entry point
+* @argc: argument count
+* @argv: argument vector
+*
+* Description: A calculator
+* Return: Always 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+	int (*fun)(int, int);
+	int res;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+
+	fun = get_op_func(argv[2]);
+	res = (*fun)(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", res);
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+/**
+* op_add - Entry point
+* @a: number
+* @b: number
+*
+* Description: Add 2 numbers
+* Return: Resault of add
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+* op_sub - Entry point
+* @a: number
+* @b: number
+*
+* Description: Substract 2 numbers
+* Return: Resault of substraction
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+* op_mul - Entry point
+* @a: number
+* @b: number
+*
+* Description: Multiply 2 numbers
+* Return: Resault of multiply
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+* op_div - Entry point
+* @a: number
+* @b: number
+*
+* Description: Divide 2 numbers
+* Return: Resault of divide
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+* op_mod - Entry point
+* @a: number
+* @b: number
+*
+* Description: Calculate the rest of a divide of 2 number
+* Return: Resault of calc
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
