a54cfb74fd09580397220dab8df14ceb49f98049
Diego Varela <2806@holbertonschool.com>
2021-03-10 15:53:49 +0000

    Fix task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
-	if (size != 0)
+	if ((size != 0) && (array != 0) && (action != 0))
 	{
 		for (i = 0 ; i < size ; i++)
 		{
