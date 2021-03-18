939bf49adf4218ad17215eb0bf04abd0f2671d0f
Diego Varela <2806@holbertonschool.com>
2021-03-18 20:52:54 +0000

    Fix task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -12,16 +12,18 @@ void free_list(list_t *head)
 	list_t *aux = head;
 	list_t *aux_next;
 
-
-	aux_next = head->next;
-	while (aux_next)
+	if (head)
 	{
+		aux_next = head->next;
+		while (aux_next)
+		{
+			free(aux->str);
+			free(aux);
+			aux = aux_next;
+			aux_next = aux_next->next;
+		}
 		free(aux->str);
 		free(aux);
-		aux = aux_next;
-		aux_next = aux_next->next;
 	}
-	free(aux->str);
-	free(aux);
 
 }
