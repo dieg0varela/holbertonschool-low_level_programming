f268f9dc4e3e86f8c5ffef8612d1637360d7d68e
Diego Varela <2806@holbertonschool.com>
2021-03-23 21:38:15 +0000

    Upload task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* delete_nodeint_at_index - Entry point
+* @head: Pointer to head of the list
+* @idx: place to the list you want to insert the new node
+*
+* Description: Delete a node in the idx place of the list
+* Return: 1 success -1 error
+*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *aux = *head;
+	listint_t *aux_prev;
+	unsigned int i = 0;
+
+	if (index == 0)
+	{
+		if (*head)
+		{
+			*head = aux->next;
+			free(aux);
+		}
+		else
+			return (-1);
+		return (1);
+	}
+	if (aux)
+	{
+		while (aux && i != index)
+		{
+			aux_prev = aux;
+			aux = aux->next;
+			i++;
+		}
+		if (i == index)
+		{
+			aux_prev->next = aux->next;
+			free(aux);
+			return (1);
+		}
+		else
+			return (-1);
+	}
+	return (-1);
+}
