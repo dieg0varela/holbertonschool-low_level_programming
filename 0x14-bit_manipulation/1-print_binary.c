e567f13d5ec7496e1518888d1d315c37ce1c9a43
Diego Varela <2806@holbertonschool.com>
2021-03-26 02:53:27 +0000

    Fix task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
-* print_binary - Entry point
+* print_binary_r - Entry point
 * @n: Number to print in binary
 *
 * Description: Print the given number in binary
 * Return: nothing
 */
-void print_binary(unsigned long int n)
+void print_binary_r(unsigned long int n)
 {
 	unsigned long int i = 0;
 
@@ -17,4 +17,18 @@ void print_binary(unsigned long int n)
 		print_binary(n >> 1);
 		_putchar(i + '0');
 	}
+}
+/**
+* print_binary - Entry point
+* @n: NUmber
+*
+* Description: Print number in binary
+* Return: Nothing
+*/
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		_putchar('0');
+	else
+		print_binary_r(n);
 }
