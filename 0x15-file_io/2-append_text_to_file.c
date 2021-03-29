8e542d6e7aabf5599940b00342fa01a0b6aef120
Diego Varela <2806@holbertonschool.com>
2021-03-29 20:34:54 +0000

    Upload task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+* create_file - Entry point
+* @filename: Pointer to file
+* @text_content: Content to insert in the file
+*
+* Description: Create a new file with the given content
+* Return: 1 Success or -1 Fail
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd = 0, pichu = 0;
+
+	if (!filename)
+		return (-1);
+	if (!text_content)
+		return (1);
+
+	fd = open(filename, O_APPEND | O_WRONLY);
+	if (fd == -1)
+		return (-1);
+	while (text_content[pichu] != '\0')
+	{
+		pichu++;
+	}
+	if ((write(fd, text_content, pichu)) == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
