c02d980c0f0d15a62db4d26b7018869b6e70c623
Diego Varela <2806@holbertonschool.com>
2021-03-29 19:31:48 +0000

    Fix task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,8 +13,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+	if (!text_content)
+		text_content = "";
 
-	fd = open(filename, O_CREAT |O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	while (text_content[pichu] != '\0')
