d8918eefcbab65a036ae6b4433c56fe2dd944413
Diego Varela <2806@holbertonschool.com>
2021-05-02 20:09:59 +0000

    Fix task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	if (!head)
 		return (NULL);
-	for (i = 0 ; copy->next != '\0' ; i++)
+	for (i = 0 ; copy != '\0' ; i++)
 	{
 		if (i == index)
 			return (copy);
