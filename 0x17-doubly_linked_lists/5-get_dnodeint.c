83dea46e73e102331684a49dbf9a4fa2ddb7c197
Diego Varela <2806@holbertonschool.com>
2021-05-02 20:07:40 +0000

    Upload task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,4 +1,12 @@
 #include "lists.h"
+/**
+* get_dnodeint_at_index - Entry point
+* @head: pointer to head
+* @index: index to return
+*
+* Description: Return the node in index place
+* Return: node in index place
+*/
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
