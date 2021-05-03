8346db1fc70c8756b23bfd2ce938492b34eb0c3c
Diego Varela <2806@holbertonschool.com>
2021-05-03 02:35:36 +0000

    Upload tasks 7 and 102

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,24 @@
 #include "lists.h"
 /**
+* list_len - Calculate the length of the list
+* @h: pointer to head
+*
+* Return: size
+*/
+unsigned int list_len(dlistint_t *h)
+{
+	int i = 0;
+	dlistint_t *copy = h;
+
+	while (copy)
+	{
+		i++;
+		copy = copy->next;
+	}
+	return (i);
+}
+
+/**
 * insert_dnodeint_at_index - Entry point
 * @h: pinter to head
 * @idx: index to add node
@@ -15,25 +34,33 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (!h)
 		return (NULL);
+	if (list_len(*h) == idx)
+		return (add_dnodeint_end(h, n));
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
 	if (idx == 0)
 	{
-		new_node->prev = NULL;
-		new_node->next = copy->next;
+		if (!(*h))
+		{
+			*h = new_node;
+			return (new_node);
+		}
+		new_node->next = copy;
 		copy->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
-	for (i = 0 ; copy != '\0' ; i++)
+	for (i = 0 ; copy != '\0' || i == idx ; i++)
 	{
 		if (i == idx)
 		{
 			new_node->next = copy;
 			new_node->prev = copy->prev;
-			(copy->prev)->next = new_node;
+			copy->prev->next = new_node;
 			copy->prev = new_node;
 			return (new_node);
 		}
