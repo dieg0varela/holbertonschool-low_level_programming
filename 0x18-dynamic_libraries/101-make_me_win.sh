06de3e2c7b751a3079d1f7c5265731fd17325e83
Diego Varela <2806@holbertonschool.com>
2021-05-04 03:15:48 +0000

    Delete white space task 101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/dieg0varela/holbertonschool-low_level_programming/raw/main/0x18-dynamic_libraries/win.so
+wget -q -P /tmp/ https://github.com/dieg0varela/holbertonschool-low_level_programming/raw/main/0x18-dynamic_libraries/win.so
 export LD_PRELOAD=/tmp/win.so
-
