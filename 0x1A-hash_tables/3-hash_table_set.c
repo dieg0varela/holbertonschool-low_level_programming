db9caa851048707a2018bd966822f19b5268f8a1
Diego Varela <2806@holbertonschool.com>
2021-05-28 19:24:54 +0000

    fix task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -61,7 +61,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	char *val;
 
-	if (!ht || !key || !value)
+	if (!ht || !key || !value || !ht->array)
 		return (0);
 	if (strcmp(key, "") == 0)
 		return (0);
