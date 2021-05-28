f55cb1cde30abf0f12ae3702be955b2a39c2efb2
Diego Varela <2806@holbertonschool.com>
2021-05-28 20:59:54 +0000

    Upload task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+* hash_table_print - Entry point
+* @ht: hash table
+*
+* Description: Print a hash table
+* Return: Nothing
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int size = ht->size, i = 0;
+	hash_node_t *temp;
+	int fake_bool = 0;
+
+	printf("{");
+	for (i = 0 ; i < size ; i++)
+	{
+		temp = ht->array[i];
+		while (temp)
+		{
+			if (fake_bool == 1)
+				printf(", ");
+			printf("'%s' : '%s'", temp->key, temp->value);
+			fake_bool = 1;
+			temp = temp->next;
+		}
+	}
+	printf("}\n");
+}
