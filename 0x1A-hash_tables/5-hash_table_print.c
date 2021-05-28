f6c4f708d28e3f25c35a83f0379454d7683be2e3
Diego Varela <2806@holbertonschool.com>
2021-05-28 21:01:41 +0000

    Fix space task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -20,7 +20,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (fake_bool == 1)
 				printf(", ");
-			printf("'%s' : '%s'", temp->key, temp->value);
+			printf("'%s': '%s'", temp->key, temp->value);
 			fake_bool = 1;
 			temp = temp->next;
 		}
